72b2c89d9d16fe39030c2fba6701897be83ff100
Kartikey-051107 <Kartikey05gupta@gmail.com>
2026-06-05 13:30:08 +0530

    area of rectangle using tuples

---
+++ struct_/src/area1.rs
@@ -0,0 +1,10 @@
+fn area(width:u32,hieght:u32)->u32{
+    width*hieght
+}
+fn main(){
+    let width=30;
+    let hieght=50;
+
+    println!("the area of rectangle is {}",area(width,hieght));
+    
+}
